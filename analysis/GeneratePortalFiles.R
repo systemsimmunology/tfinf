078b81c10757b2fdda09df20a930f06b8bb40419
thorsson <thorsson@5021c4a6-a05a-4b22-8634-7b27b77db61f>
2010-07-09 23:36:26 +0000

    git-svn-id: http://svn.systemsbiology.net/repos/compbio/Vesteinn_scripts/tfinf/trunk@9876 5021c4a6-a05a-4b22-8634-7b27b77db61f

--- analysis/GeneratePortalFiles.R
+++ analysis/GeneratePortalFiles.R
@@ -12,11 +12,18 @@ delta.t <- c(20.,20.,20.,20.,40.,120.,120.,120.,600.,360.)
 array.times <- c(0,20,40,60,80,120,240,360,480,1080,1440)
 cc <- cname.compare
 
+## June 2010 - Cytokines
+if (FALSE){
 ca.ps <- as.character(repProbes.ncbiID[as.character(read.table("/Users/thorsson/data/GeneOntology/CytokineActivity.tsv")$V1)])
 modvec <- sliceModVecByTargs(ca.ps,modvec.e)
 predPlotWrapperModVec(modvec,savedir="JUNE2010")
-
 createModPropFileMV(modvec,"ModelAttributes.tsv")
 createEdgeFileMV(modvec,"EdgeAttributes.tsv")
-
 createModfileLookupFileMV(modvec,"ProfileImages.txt","/proj/ilyalab/Vesteinn/forHector/ProfilePlots","png")
+}
+## July 2010 - Full models
+modvec <- modvec.e
+createModPropFileMV(modvec,"ModelAttributes.tsv")
+createEdgeFileMV(modvec,"EdgeAttributes.tsv")
+createModfileLookupFileMV(modvec,"ProfileImages.txt","/proj/ilyalab/Vesteinn/forAndreaAndDick/ProfilePlots","png")
+predPlotWrapperModVec(modvec,savedir="ProfilePlots")
