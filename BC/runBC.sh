e3ab2ebde6b80544de0d1c6c64284736cc160d10
thorsson <thorsson@5021c4a6-a05a-4b22-8634-7b27b77db61f>
2011-11-30 22:02:20 +0000

    git-svn-id: http://svn.systemsbiology.net/repos/compbio/Vesteinn_scripts/tfinf/trunk@10145 5021c4a6-a05a-4b22-8634-7b27b77db61f

--- BC/runBC.sh
+++ BC/runBC.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-R --no-save < find.pairs_allMouseBC.R >& find.pairs_allMouseBC.logfile
-R --no-save < randEScube.R >& randEScube.logfile
-R --no-save < GetFeatureListSingles.R >& GetFeatureListSingles.logfile
-R --no-save < GetFeatureListPairs.R >&  GetFeatureListPairs.logfile
-R --no-save < metacollectionWrapperBC.R >& metacollectionWrapperBC.logfile
-R --no-restore --no-save < createTargsAndsCandsBC.R >& createTargsAndsCandsBC.logfile
+R --no-save < find.pairs_allMouseBC.R > find.pairs_allMouseBC.logfile
+R --no-save < randEScube.R > randEScube.logfile
+R --no-save < GetFeatureListSingles.R > GetFeatureListSingles.logfile
+R --no-save < GetFeatureListPairs.R >  GetFeatureListPairs.logfile
+R --no-save < metacollectionWrapperBC.R > metacollectionWrapperBC.logfile
+cd ../R
+R --no-restore --no-save < createTargsAndsCandsBC.R > createTargsAndsCandsBC.logfile
